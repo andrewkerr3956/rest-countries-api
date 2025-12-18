85850e15758f60d663d51c4a4238b42a3a873729
Andrew Kerr <andrewkerr3956@gmail.com>
2025-12-18 14:09:46 -0500

    progress

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,31 @@
 import { Suspense, use, useEffect, useRef, useState } from "react";
 import Filter from "../components/ui/Filter/Filter";
 import SearchInput from "../components/ui/SearchInput/SearchInput";
+import CountryCard from "../components/ui/CountryCard/CountryCard";
 
 export default function HomePage() {
-    const initialCountriesData = useRef(() => handleFetchCountries());
+    const initialCountriesData = useRef([]);
 
     const [countriesData, setCountriesData] = useState([]);
 
-    const handleFetchCountries = async() => {
+    const handleFetchCountries = async () => {
         return fetch('https://restcountries.com/v3.1/all?fields=capital,flag,name,population,region').then(resp => resp.json());
     }
+
+    useEffect(() => {
+        (async () => {
+            const data = await handleFetchCountries();
+            console.log(data)
+            setCountriesData(data)
+        })();
+    }, []);
 
     return (
         <div>
             <SearchInput
                 placeholder='Search for a country...'
             />
-            <Filter 
+            <Filter
                 options={[
                     { label: 'Africa', value: 'africa' },
                     { label: 'America', value: 'america' },
@@ -26,9 +35,18 @@ export default function HomePage() {
                 ]}
             />
             <Suspense>
-                {countriesData && countriesData?.length > 0 ? countriesData.map((c) => (
-                    <div>Country</div>
-                )) : null}
+                <div className="countriesGrid">
+                    {countriesData && countriesData?.length > 0 ? countriesData.map((c) => (
+                        <CountryCard 
+                            key={c?.name?.official}
+                            country={c?.name?.official}
+                            region={c?.region}
+                            capital={c?.capital?.[0]}
+                            population={c?.population}
+                            flag={c?.flag}
+                        />
+                    )) : null}
+                </div>
             </Suspense>
         </div>
     )
