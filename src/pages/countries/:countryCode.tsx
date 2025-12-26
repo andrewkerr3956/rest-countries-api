fcfd60485a4787b6c652de0a7423d7f814dc7b40
Andrew Kerr <andrewkerr3956@gmail.com>
2025-12-26 17:18:18 -0500

    progress

--- src/pages/countries/:countryCode.tsx
+++ src/pages/countries/:countryCode.tsx
@@ -1,16 +1,28 @@
 import { Suspense, use, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 
-function CountryDetails({ code }: { code: string }) {
-    const handleFetchDetails = async (code: string) => {
-        return fetch(`https://restcountries.com/v3.1/alpha/${code}`).then(resp => resp.json());
-    }
+const handleFetchDetails = async (code: string) => {
+    const data = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+    return data?.json();
+}
 
+function CountryDetails({ code }: { code: string }) {
     const data = use(handleFetchDetails(code));
 
     return (
         <div>
-            <h1 style={{ color: 'white' }}>{data?.[0]?.name?.official}</h1>
+            <picture>
+                <img alt="Country Flag" src={data?.[0]?.flags?.png} />
+            </picture>
+            <h1 style={{ color: 'white' }}>{data?.[0]?.name?.common}</h1>
+            <p>Native Name: {data?.[0]?.name?.nativeName?.[code?.toLowerCase()]?.official}</p>
+            <p>Population: {data?.[0]?.population}</p>
+            <p>Region: {data?.[0]?.region}</p>
+            <p>Sub Region: {data?.[0]?.subregion}</p>
+            <p>Capital: {data?.[0]?.capital}</p>
+            <p>Top Level Domain: {data?.[0]?.tld?.[0]}</p>
+            <p>Currencies: {Object.keys(data?.[0]?.currencies)?.[0]}</p>
+            <p>Languages: {Object.values(data?.[0]?.languages)?.map((v) => <span style={{ textTransform: 'capitalize' }}>{v}</span>)}</p>
         </div>
     )
 }
